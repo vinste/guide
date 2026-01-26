37de9a308895a7e3bd2878777d4ab666f423051d
Vincent Colas <vincent.colas@me.com>
2026-01-26 10:28:28 +0000

    titre de-fr

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -29,12 +29,20 @@ export default function Navbar() {
           <div className="flex-shrink-0 flex items-center">
             <Link href="/" className="flex flex-col">
               <span className="font-display text-2xl font-bold text-primary tracking-wide">
-                Amandine Guide
-                <br className="sm:hidden lg:block" />
-                Conférencière
+                {language === "fr" ? (
+                  <>
+                    Amandine Guide <br className="sm:hidden lg:block" />
+                    Conférencière
+                  </>
+                ) : (
+                  <>
+                    Amandine <br className="sm:hidden lg:block" />
+                    Reiseleiterin
+                  </>
+                )}
               </span>
               <span className="text-xs uppercase tracking-widest text-muted-foreground">
-                Lyon, Beaujolais & Bourgogne
+                Lyon, Beaujolais & {language === "fr" ? "Bourgogne" : "Burgund"}
               </span>
             </Link>
           </div>
